efbc14345792a58e4069b2471c4d333f5687039c
mangming2 <leejiho35@gmail.com>
2024-12-17 11:43:43 +0900

    🔥 : delete unused components

--- src/pages/Routes.tsx
+++ src/pages/Routes.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainPage from "./main-page";
-import ProtectedRoute from "../components/ProtectedRoute";
 
 const router = createBrowserRouter([
 
