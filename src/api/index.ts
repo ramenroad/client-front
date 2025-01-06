02106209a5b8b3cb251de9175488487b5d720bfe
mangming2 <leejiho35@gmail.com>
2025-01-06 22:46:30 +0900

    update : baseurl

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export const instance = axios.create({
-    //baseURL: "http://52.78.143.51/v1",
-    baseURL: "http://localhost:3000/v1",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-  });
+  baseURL: "http://52.78.143.51/v1",
+  //baseURL: "http://localhost:3000/v1",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
