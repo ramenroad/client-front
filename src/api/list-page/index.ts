546bb650c2d705020afa88697d95f15d3f51a038
mangming2 <leejiho35@gmail.com>
2025-01-13 00:19:22 +0900

    🔥 : hotfix conflict error

--- src/api/list-page/index.ts
+++ src/api/list-page/index.ts
@@ -1,20 +1,9 @@
 import { instance } from "../index";
 import { Ramenya } from "../../types";
 
-
-const instance = axios.create({
-  baseURL: "https://ramenroad.com/api/v1",
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-  },
-});
-
-
 export const getRamenyaList = async (location: string) => {
   const response = await instance.get<Ramenya[]>(`/ramenya/all`, {
     params: { region: location },
   });
   return response.data;
 };
-
