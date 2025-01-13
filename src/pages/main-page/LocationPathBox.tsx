2690dc6af18defec59b10f1d8fb78b9a57da185e
mangming2 <leejiho35@gmail.com>
2025-01-13 20:14:17 +0900

    💄 폰트 추가 + 인스타 링크 피드백 반영

--- src/pages/main-page/LocationPathBox.tsx
+++ src/pages/main-page/LocationPathBox.tsx
@@ -1,40 +1,28 @@
-import { useNavigate } from "react-router-dom";
-import tw from "twin.macro";
-import { useMobileState } from "../../hooks/useMobileState.ts";
-import styled from "@emotion/styled/macro";
+import { useNavigate } from 'react-router-dom';
+import tw from 'twin.macro'
 
 interface LocationPathBoxProps {
-  location: string;
+    location: string;
 }
 
-export const LocationPathBox = ({ location }: LocationPathBoxProps) => {
-  const navigate = useNavigate();
-  const { isMobile } = useMobileState();
-
+export const LocationPathBox = ({location}: LocationPathBoxProps) => {
+const navigate = useNavigate();
   return (
-    <Wrapper
-      isMobile={isMobile}
-      onClick={() => navigate(`/location/${location}`)}
-    >
-      <LocationPathText>{location}</LocationPathText>
+    <Wrapper onClick={() => navigate(`/location/${location}`)}>
+        <LocationPathText>{location}</LocationPathText>
     </Wrapper>
-  );
-};
-
-interface WrapperProps {
-  isMobile: boolean;
+  )
 }
 
-const Wrapper = styled.div<WrapperProps>(({ isMobile }) => [
-  tw`
-    flex items-center justify-center
-    border-solid border-1 border-divider
-    rounded-4
-    w-110 h-50
-    cursor-pointer
-  `,
-  !isMobile && tw`hover:(bg-orange text-white) active:(bg-orange text-white)`,
-]);
+const Wrapper = tw.div`
+  flex items-center justify-center
+  border-solid border-1 border-divider
+  rounded-4 
+  w-110 h-50
+  cursor-pointer
+  hover:(bg-orange text-white)
+  active:(bg-orange text-white)
+`;
 
 const LocationPathText = tw.div`
   flex font-14-r 
