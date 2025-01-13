c0953f63f3021278cba75f8ffed6cf840e357e0b
mangming2 <leejiho35@gmail.com>
2025-01-13 19:53:34 +0900

    ✨ : change baseurl

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  //baseURL: "https://ramenroad.com/api/v1",
-  baseURL: "http://localhost:3000/v1",
+  baseURL: "https://ramenroad.com/api/v1",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
