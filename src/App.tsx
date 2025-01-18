3ba9f71dcffbce378610666b3bb26006ef2f8f30
디디 <developer.cdd@gmail.com>
2025-01-18 20:03:02 +0900

    💄: 전체 테두리 및 너비 공통 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Routes } from "./pages/Routes.tsx";
 import { queryClient } from "./core/queryClient";
+import tw from "twin.macro";
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Routes />
+      <Screen>
+        <View>
+          <Routes />
+        </View>
+      </Screen>
     </QueryClientProvider>
   );
 }
+
+const Screen = tw.section`
+  flex justify-center min-h-screen overflow-hidden box-border
+`;
+
+const View = tw.main`
+  flex flex-col items-center box-border
+  w-390 h-full
+  border-0 border-x border-border border-solid
+`;
 
 export default App;
