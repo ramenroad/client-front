06cda103a423db6f9a2ff3adb8ba595bf1f0e82c
mangming2 <leejiho35@gmail.com>
2025-01-23 14:42:13 +0900

    ✨ : footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Routes } from "./pages/Routes.tsx";
 import { queryClient } from "./core/queryClient";
 import tw from "twin.macro";
-
+import { Footer } from "./components/common/Footer.tsx";
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Screen>
         <View>
           <Routes />
+          <Footer />
         </View>
       </Screen>
+
     </QueryClientProvider>
   );
 }
