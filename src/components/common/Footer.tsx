b00f6760ccce8640d916287804136cc2c663a472
디디 <developer.cdd@gmail.com>
2025-01-23 23:45:50 +0900

    ✨: Footer 구현

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,12 +1,37 @@
-// import tw from "twin.macro";
+import tw from "twin.macro";
+import { ramenroadEmail } from "../../constants";
 
 export const Footer = () => {
-  return null;
-  // return <Wrapper>Footer</Wrapper>;
+  // return null;
+  return (
+    <Wrapper>
+      <Title>팀 라멘로드</Title>
+      <ContactWrapper>
+        <span>제휴 및 협업 문의</span>
+        <Email href={`mailto:${ramenroadEmail}`}>{ramenroadEmail}</Email>
+      </ContactWrapper>
+      <Description>Copyright © 2025 라멘로드 All rights reserved.</Description>
+    </Wrapper>
+  );
 };
 
-// const Wrapper = tw.div`
-//   w-full h-20
-//   bg-orange text-white
-//   text-center
-// `;
+const Wrapper = tw.div`
+  w-full h-134 p-20 box-border
+  bg-footer text-gray-500 font-14-r
+`;
+
+const Title = tw.div`
+  font-14-sb
+`;
+
+const Email = tw.a`
+  font-14-m link:text-gray-500 no-underline
+`;
+
+const ContactWrapper = tw.div`
+  flex gap-8 mt-4 mb-24
+`;
+
+const Description = tw.div`
+  font-12-r
+`;
