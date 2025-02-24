d7e32d0e9d6b2e39a537aa58b7e8a761de7c4086
mangming2 <leejiho35@gmail.com>
2025-02-24 11:36:43 +0900

    🔥 : del console

--- src/api/detail-page/index.ts
+++ src/api/detail-page/index.ts
@@ -3,6 +3,5 @@ import { RemenyaDetail } from "../../types";
 
 export const getRamenyaDetail = async (id: string) => {
   const response = await instance.get<RemenyaDetail>(`/ramenya/${id}`);
-  console.log(response.data);
   return response.data;
 };
