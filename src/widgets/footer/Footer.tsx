ac258ffceff6706442bccbb9d5dbf10948577911
devcdd <developer.cdd@gmail.com>
2026-06-13 12:10:45 +0900

    chore: 푸터 저작권 연도 갱신

--- src/widgets/footer/Footer.tsx
+++ src/widgets/footer/Footer.tsx
@@ -1,13 +1,13 @@
-import { EXTERNAL_LINKS, ramenroadEmail } from '@/shared/config'
-import { IconInstagram } from '@/shared/ui/icon'
-import { Line } from '@/shared/ui/line'
-import render from '@/shared/ui/render'
-import { RaisingText } from '@/shared/ui/text'
+import { EXTERNAL_LINKS, ramenroadEmail } from "@/shared/config";
+import { IconInstagram } from "@/shared/ui/icon";
+import { Line } from "@/shared/ui/line";
+import render from "@/shared/ui/render";
+import { RaisingText } from "@/shared/ui/text";
 
-const SERVICE_TERMS_URL = EXTERNAL_LINKS.serviceTerms
-const PRIVACY_POLICY_URL = EXTERNAL_LINKS.privacyPolicy
-const LOCATION_POLICY_URL = EXTERNAL_LINKS.locationPolicy
-const INSTAGRAM_URL = 'https://www.instagram.com/ra.ising/'
+const SERVICE_TERMS_URL = EXTERNAL_LINKS.serviceTerms;
+const PRIVACY_POLICY_URL = EXTERNAL_LINKS.privacyPolicy;
+const LOCATION_POLICY_URL = EXTERNAL_LINKS.locationPolicy;
+const INSTAGRAM_URL = "https://www.instagram.com/ra.ising/";
 
 export const Footer = () => {
   return (
@@ -24,52 +24,76 @@ export const Footer = () => {
 
       <TermsAndConditions>
         <Terms>
-          <Link href={SERVICE_TERMS_URL} target="_blank" rel="noopener noreferrer">
+          <Link
+            href={SERVICE_TERMS_URL}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             서비스 이용 약관
           </Link>
           <Divider vertical />
-          <StrongLink href={PRIVACY_POLICY_URL} target="_blank" rel="noopener noreferrer">
+          <StrongLink
+            href={PRIVACY_POLICY_URL}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             개인정보 처리방침
           </StrongLink>
           <Divider vertical />
-          <Link href={LOCATION_POLICY_URL} target="_blank" rel="noopener noreferrer">
+          <Link
+            href={LOCATION_POLICY_URL}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             위치 정보 이용약관
           </Link>
         </Terms>
         <RaisingText size={12} weight="r">
-          Copyright © 2025 라이징 All rights reserved.
+          Copyright © 2026 라이징 All rights reserved.
         </RaisingText>
       </TermsAndConditions>
 
-      <InstagramLink href={INSTAGRAM_URL} target="_blank" rel="noopener noreferrer" aria-label="라이징 인스타그램">
+      <InstagramLink
+        href={INSTAGRAM_URL}
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="라이징 인스타그램"
+      >
         <InstagramIcon footer />
       </InstagramLink>
     </Wrapper>
-  )
-}
+  );
+};
 
-const Wrapper = render.div('w-full h-212 p-20 box-border bg-footer text-gray-500 font-14-r flex flex-col gap-24')
+const Wrapper = render.div(
+  "w-full h-212 p-20 box-border bg-footer text-gray-500 font-14-r flex flex-col gap-24",
+);
 
-const FooterTitle = render.extend(RaisingText, 'leading-27')
+const FooterTitle = render.extend(RaisingText, "leading-27");
 
-const Contact = render.div('flex flex-col gap-4')
+const Contact = render.div("flex flex-col gap-4");
 
-const ContactLabel = render.span('text-inherit')
+const ContactLabel = render.span("text-inherit");
 
-const Email = render.a('font-14-m text-gray-500 no-underline')
+const Email = render.a("font-14-m text-gray-500 no-underline");
 
-const ContactWrapper = render.div('flex gap-8')
+const ContactWrapper = render.div("flex gap-8");
 
-const Terms = render.div('flex gap-4 items-center')
+const Terms = render.div("flex gap-4 items-center");
 
-const TermsAndConditions = render.div('flex flex-col gap-4')
+const TermsAndConditions = render.div("flex flex-col gap-4");
 
-const Link = render.a('font-12-r cursor-pointer text-gray-500 no-underline')
+const Link = render.a("font-12-r cursor-pointer text-gray-500 no-underline");
 
-const StrongLink = render.a('font-12-sb cursor-pointer text-gray-500 no-underline')
+const StrongLink = render.a(
+  "font-12-sb cursor-pointer text-gray-500 no-underline",
+);
 
-const InstagramLink = render.a('w-fit')
+const InstagramLink = render.a("w-fit");
 
-const InstagramIcon = render.extend(IconInstagram, 'min-h-40 min-w-40 mt-[-8px]')
+const InstagramIcon = render.extend(
+  IconInstagram,
+  "min-h-40 min-w-40 mt-[-8px]",
+);
 
-const Divider = render.extend(Line, 'h-10')
+const Divider = render.extend(Line, "h-10");
